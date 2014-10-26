3b6b603f7cb40c449f19264d85ae34da19ec460c
NetPuter <netputer@gmail.com>
2014-10-26 19:30:18 +0800

    feat: Available page

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,75 @@ router.get('/', function (req, res) {
     });
 });
 
+router.get('/room/available', function (req, res) {
+    async.waterfall([function (callback) {
+        var debug = false;
+        debug = true;
+
+        if (!!debug) {
+            return callback(null, require('../config/available.json'));
+        }
+
+        var roomIds = _.map(meetingRooms, function (room) {
+            return {
+                id: room.id
+            };
+        });
+
+        calendar.freebusy.query({
+            auth: authClient,
+            fields: 'calendars',
+            resource: {
+                calendarExpansionMax: roomIds.length,
+                items: roomIds,
+                // timeMin: '2014-10-24T14:00:00+08:00',
+                // timeMax: '2014-10-24T16:00:00+08:00',
+                timeMin: moment().format(),
+                timeMax: moment().add(1, 'h').format(),
+                timeZone: 'Asia/Shanghai'
+            }
+        }, function (error, resp) {
+            console.log(resp);
+
+            if (error) {
+                return callback(error.message);
+            }
+
+            callback(null, resp.calendars);
+        });
+    }, function (calendars, callback) {
+        var roomReindex = {};
+        var result = [];
+
+        _.each(meetingRooms, function (room) {
+            roomReindex[room.id] = room;
+        });
+
+        _.each(calendars, function (room, key) {
+            if (room.busy.length === 0) {
+                result.push(roomReindex[key]);
+            }
+        });
+
+        var rooms = _.sortBy(result, function (room) {
+            return room.name;
+        });
+
+        callback(null, rooms);
+    }], function (error, rooms) {
+        if (error) {
+            var err = new Error(error);
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('available', {
+            title: 'Available within one hour',
+            rooms: rooms
+        });
+    });
+});
+
 router.get('/room/:name', function (req, res, next) {
     var name = req.params.name.toLowerCase();
     var room = meetingRooms[name];
