4a31e23a6308d1742ddbcd1e54a32231950f7188
NetPuter <netputer@gmail.com>
2014-10-26 19:29:47 +0800

    feat: Room page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var google = require('googleapis');
+var calendar = google.calendar('v3');
 
+var async = require('async');
+var moment = require('moment');
 var _ = require('lodash');
 
 var config = require('../config');
@@ -29,6 +32,62 @@ router.get('/', function (req, res) {
         rooms: _.sortBy(meetingRooms, function (room) {
             return room.name;
         })
+    });
+});
+
+router.get('/room/:name', function (req, res, next) {
+    var name = req.params.name.toLowerCase();
+    var room = meetingRooms[name];
+
+    if (!room) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        return next(err);
+    }
+
+    async.waterfall([function (callback) {
+        var debug = false;
+        debug = true;
+
+        if (debug) {
+            return callback(null, require('../config/events.json'));
+        }
+
+        calendar.events.list({
+            auth: authClient,
+            calendarId: room.id,
+            fields: 'items(attendees,attendeesOmitted,creator,description,end,id,organizer,start,summary)',
+            orderBy: 'startTime',
+            singleEvents: true,
+            // timeMin: '2014-10-24T00:00:00+08:00',
+            // timeMax: '2014-10-25T00:00:00+08:00',
+            timeMin: moment().format(),
+            timeMax: moment().format('YYYY-MM-DDT23:59:59+08:00'),
+            timeZone: 'Asia/Shanghai'
+        }, function (error, resp) {
+            if (error) {
+                return callback(error.message);
+            }
+
+            // require('fs').writeFile('config/events.json', JSON.stringify(resp.items), function (fsError) {
+            //     if (fsError) {
+            //         return callback(fsError);
+            //     }
+            // });
+
+            callback(null, resp.items);
+        });
+    }], function (error, events) {
+        if (error) {
+            var err = new Error(error);
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('room', {
+            title: room.name,
+            events: events
+        });
     });
 });
 
