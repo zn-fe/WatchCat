2d0f3a646dcb221926c09989610a138d99d86ed6
NetPuter <netputer@gmail.com>
2015-01-18 18:16:13 +0800

    fix: Only tracking under `wandoulabs.com` domain

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -62,7 +62,7 @@ $(function () {
     setTimeout(function () {
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-15790641-63', 'auto');
+        ga('create', 'UA-15790641-63', 'wandoulabs.com');
         ga('send', 'pageview');
     }, 200);
     // jshint ignore:end
