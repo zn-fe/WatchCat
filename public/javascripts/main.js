ea9ef1176315b84ad85879b023c8d2bfce220415
NetPuter <netputer@gmail.com>
2015-01-18 18:09:07 +0800

    refactor: Move tracking code into `main.js`

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -57,4 +57,13 @@ $(function () {
 
     $btnBook.toggleClass('disabled', !$inputUsername.val() || !$inputDescription.val());
     $modalStatus.show();
+
+    // jshint ignore:start
+    setTimeout(function () {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-15790641-63', 'auto');
+        ga('send', 'pageview');
+    }, 200);
+    // jshint ignore:end
 });
