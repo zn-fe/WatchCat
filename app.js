4ea7580982293ae3ae1fea1cb182058358e01fa8
NetPuter <netputer@gmail.com>
2015-01-18 17:41:10 +0800

    refactor: Remove useless modules

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var hbs = require('hbs');
 
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -21,8 +19,6 @@ hbs.registerHelper('moment', function (context, block) {
     return moment(context).format(format);
 });
 
-// app.use(favicon(__dirname + '/public/favicon.ico'));
-// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
